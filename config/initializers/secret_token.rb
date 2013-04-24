e1dd5ef6fa131428625a7c7195f4ee691a5011cc
youngsikkim <youngsik.kim@headflow.net>
2013-04-24 10:10:01 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concept2::Application.config.secret_token = 'a0712c88c7653516128f51398bd3cede9e653928a0f9eed223510466e550bf16d6a867aa2cbafaff9dc1d098af3a261aeae00e62d20bea9eff1acd8d5051a01a'
